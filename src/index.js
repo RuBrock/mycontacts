5c5a07c34fe9fa66571efd1bfd18c06bee04947e
RuBrock <r.brocksilva@yahoo.com.br>
2022-01-09 15:38:40 -0300

    implementing error handler with express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,5 +7,12 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+// ERROR HANDLER - Middleware modificado para interceptar erros não tratados
+app.use((error, request, response, next) => {
+  console.log('#### ERROR HANDLER');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
