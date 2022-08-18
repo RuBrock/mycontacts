b018b1fd4c63a37e745fe86f17dc875df171defb
RuBrock <r.brocksilva@yahoo.com.br>
2022-08-18 13:38:13 -0300

    CORS config and some additional Readme infos

--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+// CORS - Middleware modificado para liberar requisições vindas do Front-end
+app.use(cors);
 app.use(routes);
-
 // ERROR HANDLER - Middleware modificado para interceptar erros não tratados
-app.use((error, request, response, next) => {
-  console.log('#### ERROR HANDLER');
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
