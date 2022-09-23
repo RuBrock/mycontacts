66e5c40ea37e01f7f77489f3fc0f6e6d49efc647
RuBrock <r.brocksilva@yahoo.com.br>
2022-09-23 18:17:54 +0100

    feat: Implementing update request to edit contact

--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -35,8 +35,22 @@ export default function EditContact() {
     loadContact();
   }, [id, history]);
 
-  const handleSubmit = () => {
-    //
+  const handleSubmit = async (formData) => {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const contactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(contactData.name);
+      toast({ type: 'success', text: 'Contato alterado com sucesso!' });
+    } catch {
+      toast({ type: 'danger', text: 'Ocorreu um erro ao alterar o contato.' });
+    }
   };
 
   return (
