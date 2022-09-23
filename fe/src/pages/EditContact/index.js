821c71319483fb7fcd5d6f97e7bb2396736bcf63
RuBrock <r.brocksilva@yahoo.com.br>
2022-09-23 09:59:07 +0100

    feat: Getting contact data on edit page

--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -1,12 +1,47 @@
+import { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  const history = useHistory();
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        setIsLoading(false);
+        console.log(contactData);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  const handleSubmit = () => {
+    //
+  };
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader title="Editar Rubens Silva" />
 
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm buttonLabel="Salvar alterações" onSubmit={handleSubmit} />
     </>
   );
 }
