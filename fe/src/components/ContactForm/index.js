11df9439776efb236c5e70997741d949d8c2a96b
Rubens Silva <rusilva@tripadvisor.com>
2023-03-04 12:57:39 +0000

    fix: refactoring componentes to use custom hooks keeping the logic and the component itself the UI

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,12 +1,6 @@
 /* eslint-disable no-shadow */
-import {
-  useState, useEffect, forwardRef, useImperativeHandle,
-} from 'react';
+import { forwardRef } from 'react';
 import PropTypes from 'prop-types';
-
-import isEmailValid from '../../utils/isEmailValid';
-import formatPhone from '../../utils/formatPhone';
-import useErrors from '../../hooks/useErrors';
 
 import { Form, ButtonContainer } from './styles';
 
@@ -14,89 +8,26 @@ import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
-import CategoriesService from '../../services/CategoriesService';
-import useSafeAsyncState from '../../hooks/useSafeAsyncState';
+
+import useContactForm from './useContactForm';
 
 const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [categoryId, setCategoryId] = useState('');
-  const [categories, setCategories] = useSafeAsyncState([]);
-  const [isLoadingCategories, setIsLoadingCategories] = useSafeAsyncState(true);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
   const {
-    errors, setError, removeError, getErrorMessageByFieldName,
-  } = useErrors();
-
-  const isFormValid = (name && errors.length === 0);
-
-  // hook return what current value must be
-  useImperativeHandle(ref, () => ({
-    setFieldsValues: (contact) => {
-      setName(contact.name ?? '');
-      setEmail(contact.email ?? '');
-      setPhone(formatPhone(contact.phone ?? ''));
-      setCategoryId(contact.category.id ?? '');
-    },
-    resetFieldsValues: () => {
-      setName('');
-      setEmail('');
-      setPhone('');
-      setCategoryId('');
-    },
-  }), []);
-
-  useEffect(() => {
-    async function loadCategories() {
-      try {
-        const categoriesList = await CategoriesService.listCategories();
-
-        setCategories(categoriesList);
-      } catch {} finally {
-        setIsLoadingCategories(false);
-      }
-    }
-
-    loadCategories();
-  }, [setCategories, setIsLoadingCategories]);
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-
-    if (!event.target.value) {
-      setError({ field: 'name', message: 'Nome é obrigatório.' });
-    } else {
-      removeError('name');
-    }
-  };
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-
-    if (event.target.value && !isEmailValid(event.target.value)) {
-      setError({ field: 'email', message: 'O formato do e-mail é inválido.' });
-    } else {
-      removeError('email');
-    }
-  };
-
-  const handlePhoneChange = (event) => {
-    setPhone(formatPhone(event.target.value));
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    setIsSubmitting(true);
-
-    await onSubmit({
-      name, email, phone: phone.replace(/\D/g, ''), categoryId,
-    });
-
-    setIsSubmitting(false);
-  };
+    handleSubmit,
+    getErrorMessageByFieldName,
+    name,
+    handleNameChange,
+    isSubmitting,
+    email,
+    handleEmailChange,
+    phone,
+    handlePhoneChange,
+    isLoadingCategories,
+    categoryId,
+    setCategoryId,
+    categories,
+    isFormValid,
+  } = useContactForm(onSubmit, ref);
 
   return (
     <Form onSubmit={handleSubmit} noValidate>
---
+++ fe/src/pages/Home/useHome.js
@@ -0,0 +1,110 @@
+import {
+  useEffect, useState, useMemo, useCallback,
+} from 'react';
+
+import toast from '../../utils/toast';
+
+import ContactsService from '../../services/ContactsService';
+import useSafeAsyncState from '../../hooks/useSafeAsyncState';
+
+export default function useHome() {
+  const [contacts, setContacts] = useSafeAsyncState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useSafeAsyncState(true);
+  const [hasError, setHasError] = useSafeAsyncState(false);
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [contactBeingDeleted, setContactBeingDeleted] = useState(null);
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
+
+  const filteredContacts = useMemo(() => contacts.filter(
+    (contact) => contact.name.toLowerCase().includes(
+      searchTerm.toLowerCase(),
+    ),
+  ), [contacts, searchTerm]);
+
+  const loadContacts = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      const contactsList = await ContactsService.listContacts(orderBy);
+
+      setHasError(false);
+      setContacts(contactsList);
+    } catch {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [orderBy, setIsLoading, setHasError, setContacts]);
+
+  useEffect(() => {
+    loadContacts();
+  }, [loadContacts]);
+
+  const handleToggleOrderBy = () => {
+    setOrderBy(
+      (prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
+    );
+  };
+
+  const handleChangeSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleTryAgain = () => {
+    loadContacts();
+  };
+
+  const handleDeleteContact = (contact) => {
+    setContactBeingDeleted(contact);
+    setIsDeleteModalVisible(true);
+  };
+
+  const handleCloseDeleteModal = () => {
+    setIsDeleteModalVisible(false);
+    setContactBeingDeleted(null);
+  };
+
+  const handleConfirmDeleteContact = async () => {
+    try {
+      setIsLoadingDelete(true);
+
+      await ContactsService.deleteContact(contactBeingDeleted.id);
+      setContacts((prevState) => prevState.filter((contact) => (
+        contact.id !== contactBeingDeleted.id
+      )));
+
+      handleCloseDeleteModal();
+      toast({
+        type: 'success',
+        text: 'Contato deletado com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao deletar o contato.',
+      });
+    } finally {
+      setIsLoadingDelete(false);
+    }
+  };
+
+  return {
+    isLoading,
+    isDeleteModalVisible,
+    isLoadingDelete,
+    contactBeingDeleted,
+    handleCloseDeleteModal,
+    handleConfirmDeleteContact,
+    contacts,
+    searchTerm,
+    handleChangeSearchTerm,
+    hasError,
+    filteredContacts,
+    handleTryAgain,
+    orderBy,
+    handleToggleOrderBy,
+    handleDeleteContact,
+  };
+}
